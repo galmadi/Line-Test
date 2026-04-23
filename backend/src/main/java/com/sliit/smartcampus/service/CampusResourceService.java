60590d486627006e9255040707ff1bafdee3eb1d
Member1 <dewduniweerasundara7@gmail.com>
2026-04-23 11:17:40 +0530

    Optimize queries in ResourceStatus.java

--- backend/src/main/java/com/sliit/smartcampus/service/CampusResourceService.java
+++ backend/src/main/java/com/sliit/smartcampus/service/CampusResourceService.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CampusResourceService {
 
-    private static final int MAX_CAPACITY = 5020;
+    private static final int MAX_CAPACITY = 5021;
 
     private final CampusResourceRepository campusResourceRepository;
 
