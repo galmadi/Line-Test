3f6e3e2a1dfb210a1d5bb86c1f76a8c74a643e27
Member1 <dewduniweerasundara7@gmail.com>
2026-04-23 10:46:43 +0530

    Improve CampusResourceService.java

--- backend/src/main/java/com/sliit/smartcampus/service/CampusResourceService.java
+++ backend/src/main/java/com/sliit/smartcampus/service/CampusResourceService.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CampusResourceService {
 
-    private static final int MAX_CAPACITY = 5019;
+    private static final int MAX_CAPACITY = 5020;
 
     private final CampusResourceRepository campusResourceRepository;
 
