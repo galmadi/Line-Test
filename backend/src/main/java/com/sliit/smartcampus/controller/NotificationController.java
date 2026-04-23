948b56e07a862b81d26532426e481c6cb255b1b3
Member4 <thimirathathsarana2003@gmail.com>
2026-04-23 21:43:47 +0530

    Align NotificationController.java with backend contract

--- backend/src/main/java/com/sliit/smartcampus/controller/NotificationController.java
+++ backend/src/main/java/com/sliit/smartcampus/controller/NotificationController.java
@@ -44,7 +44,7 @@ public class NotificationController {
     }
 
     @PutMapping("/read-all")
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("isAuthenticated()")
     public void markAllRead() {
         var user = currentUserService.requireCurrentUser();
